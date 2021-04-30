7645e7d0c74842bfe7e774278f094a2c24e203db
ThisALV <lel.charriere@orange.fr>
2021-04-30 17:27:52 +0200

    Moves existing testing facility into correct module

--- projects/rpt-webapp-client/src/lib/rptl-protocol.service.spec.ts
+++ projects/rpt-webapp-client/src/lib/rptl-protocol.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
-import { unexpected } from './testing-helpers';
+import { expectToBeErrored, expectToContainExactly, unexpected } from './testing-helpers';
 import { BadConnectionSubject, BadRptlMode, BadSessionState, RptlProtocolService } from './rptl-protocol.service';
-import { Observable, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { Actor } from './actor';
 import { ObjectUnsubscribedError } from 'rxjs';
 import { Availability } from './availability';
@@ -97,44 +97,6 @@ class MockedWebsocketSubject extends Subject<string> {
     }
 
     return returnedMessage;
-  }
-}
-
-
-/**
- * Expects that only error() callback of subscriber for given observable will be called.
- *
- * @param observable Observable to check state for
- * @param routine Routine that should put given observable into errored state
- */
-function expectToBeErrored(observable: Observable<any>, routine?: () => void): void {
-  let hasError = false;
-  observable.subscribe({
-    next: unexpected,
-    error: () => hasError = true,
-    complete: unexpected
-  });
-
-  // An action might be necessary to put given observable into errored state
-  if (routine !== undefined) {
-    routine();
-  }
-
-  expect(hasError).toBeTrue(); // Only error callback should have been called
-}
-
-
-/**
- * Checks for given list to contain every expected element only once, without any other elements.
- *
- * @param list Value to expect for
- * @param expected List of values to found only once inside given list
- */
-function expectToContainExactly(list: any[], ...expected: any[]): void {
-  expect(list).toHaveSize(expected.length); // Checks to not have any additional element
-
-  for (const elem of expected) { // Checks for each expected element
-    expect(list).toContain(elem);
   }
 }
 
