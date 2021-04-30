0c121c8993e15761d6875230ccd4cb22f0ca6fc6
ThisALV <lel.charriere@orange.fr>
2021-04-30 17:52:52 +0200

    Refactor: moves MockedWebsocketSubject with other testing facilities

--- projects/rpt-webapp-client/src/lib/testing-helpers.ts
+++ projects/rpt-webapp-client/src/lib/testing-helpers.ts
@@ -1,5 +1,99 @@
-import {} from 'jasmine';
-import { Observable } from 'rxjs'; // Required to use fail() declared by jasmine
+import {} from 'jasmine'; // Required to use fail() declared by Jasmine
+import { ObjectUnsubscribedError, Observable, Subject } from 'rxjs';
+
+
+/**
+ * Mocking for `WebSocketSubject` form rxjs, consisting in a Subject which next() method does not call observers next() member, and two
+ * new methods `fromServer()` and `nextMessage()` which respectively call observers next() with a message from server and poll the next
+ * message sent by the client.
+ *
+ * error() and complete() emulates a connection close frame, so complete() observers method is called no matter if error occurred or not.
+ */
+export class MockedWebsocketSubject extends Subject<string> {
+  /**
+   * Reason for connection to have been closed, if it has been closed by client (not by server)
+   */
+  closureReason?: { code: number, reason?: string };
+
+  // Queue for messages sent by client
+  private messagesQueue: string[];
+
+  constructor() {
+    super();
+    this.messagesQueue = []; // No sent messages at initialization
+  }
+
+  /**
+   * Pushes given initialized message into messages queue if subject is not completed/errored.
+   *
+   * @param value Message to push
+   *
+   * @throws ObjectUnsubscribedError if subject was unsubscribed
+   * @throws Error if message is `undefined`
+   */
+  next(value?: string): void {
+    if (value === undefined) { // Doesn't handle uninitialized value
+      throw new Error('Message must be initialized');
+    }
+
+    if (this.closed) { // Checks for subject to not have been unsubscribed
+      throw new ObjectUnsubscribedError();
+    }
+
+    if (!this.isStopped) { // Checks for subject to not have been completed/errored
+      this.messagesQueue.unshift(value); // Push back message into FIFO queue
+    }
+  }
+
+  /**
+   * Closes connection mock with given reason. It will complete this subject normally and saves the error code into `closureReason` field.
+   *
+   * @param err Error code, with required number field `code` and optional string field `reason`
+   */
+  error(err: any): void {
+    this.closureReason = err; // Saves error reason to check for it later
+    super.complete();
+  }
+
+  /**
+   * Closes connection mock normally. It will complete this subject normally and saves the no error code into `closureReason` field.
+   */
+  complete(): void {
+    this.closureReason = {code: 1000}; // No error occurred
+    super.complete();
+  }
+
+  /**
+   * Emulates a WebSocket close frame (error or not) from server by completing this subject normally and leaving `closureReason` undefined.
+   */
+  closeFromServer(): void {
+    super.complete();
+  }
+
+  /**
+   * Calls original subject `next()` method with given value to send message to client.
+   *
+   * @param value Message to be received by client
+   */
+  fromServer(value: string): void {
+    super.next(value);
+  }
+
+  /**
+   * @returns Next message inside queue, if any
+   *
+   * @throws Error if messages queue is empty
+   */
+  nextMessage(): string {
+    const returnedMessage: string | undefined = this.messagesQueue.pop();
+
+    if (returnedMessage === undefined) {
+      throw new Error('No more messages sent by client');
+    }
+
+    return returnedMessage;
+  }
+}
 
 
 export function unexpected(): void {
